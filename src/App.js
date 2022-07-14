fbb55345acf9b66df8d317fc0f37c3acfc7c67a6
PercTruman <drshawnhart@gmail.com>
2022-07-14 11:42:30 -0500

    add files after power down

--- src/App.js
+++ src/App.js
@@ -1,33 +1,41 @@
-import React from 'react';
-import NavBar from './NavBar';
-import { Routes, Route } from 'react-router-dom';
-import Home from './Home';
-import DeathGuard from './DeathGuard';
-import Drukhari from './Drukhari';
+import React, { useState, useEffect } from "react";
+import NavBar from "./NavBar";
+import { Routes, Route } from "react-router-dom";
+import Home from "./Home";
+import DeathGuard from "./DeathGuard";
+import Drukhari from "./Drukhari";
 import mortarion from "./images/mortarion.jpg";
-import AddArmyForm from './AddArmyForm';
-import AddModelForm from './AddModelForm';
-
-
-
+import AddArmyForm from "./AddArmyForm";
+import AddModelForm from "./AddModelForm";
 
 function App() {
+  const [armies, setArmies] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:9292").then((response) =>
+      response.json().then((fullArmyData) => setArmies(fullArmyData))
+    );
+  }, []);
+
   return (
-    <div style={{ backgroundImage: `url(${mortarion})`,
-    margin: '0 auto',
-    height: '100vh',
-    width: 'auto',
-    backgroundRepeat: 'no-repeat',
-    backgroundSize: 'cover'
-    }}>
+    <div
+      style={{
+        backgroundImage: `url(${mortarion})`,
+        margin: "0 auto",
+        height: "100vh",
+        width: "auto",
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+      }}
+    >
       <NavBar />
-        <Routes>
-          <Route path ="*" element={<Home />}/>
-          <Route path="/add_new_army" element={<AddArmyForm />}/>
-          <Route path="/add_new_models" element={<AddModelForm />}/>
-          <Route path="/death_guard" element={<DeathGuard />}/>
-          <Route path="/drukari" element={<Drukhari />}/>
-        </Routes >
+      <Routes>
+        <Route path="*" element={<Home />} />
+        <Route path="/add_new_army" element={<AddArmyForm />} />
+        <Route path="/add_new_models" element={<AddModelForm />} />
+        <Route path="/army/death_guard" element={<DeathGuard armies={armies} />} />
+        <Route path="/army/drukhari" element={<Drukhari />} />
+      </Routes>
     </div>
   );
 }
--- src/DeathGuard.js
+++ src/DeathGuard.js
@@ -1,12 +1,24 @@
+
 import React from 'react'
 
-function Armies() {
+function DeathGuard({armies}) {
+  fetch('http://localhost:9292/army/death_guard')
+    .then(res => res.json())
+    .then(modelList => modelList.map(model =>
+      <div>
+        <p>{model.name}</p>
+      </div>))
+  console.log(armies)
+  const armyBlockStyle = {
+    color: 'red',
+    textAlign: 'center'
+    }
 
   return (
-    <div>
-
+    <div style={armyBlockStyle}>
+      <h1 style={{armyBlockStyle, fontSize:'40px'} }>Death Guard</h1>
     </div>
   )
 }
 
-export default Armies
+export default DeathGuard;
